5f867d16f982ccd3ad3e8d249749b227b39c0358
Yerasyl2002 <shyrymbetov@gmail.com>
2023-05-23 13:10:01 +0600

    Initial Commit

---
+++ QuranFinal/QuranFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranFinalApp.swift
+//  QuranFinal
+//
+//  Created by Yerasyl Shyrymbetov on 23.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct QuranFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
